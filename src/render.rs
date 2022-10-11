dfe95fd6891e1d18c02fd5e7ca81125e1977d689
tuckspaint <tuckercumberbatch5ever@gmail.com>
2022-10-11 16:57:44 -0500

    rendering of overworld tilesprites

--- src/render.rs
+++ src/render.rs
@@ -1,32 +1,25 @@
-use sdl2::{pixels::Color, rect::Rect, render::Canvas, video::Window};
+use sdl2::{pixels::Color, rect::Rect, render::Canvas, video::Window, image::LoadTexture};
 
 use crate::{player, TILE_SIZE};
 
 pub fn render(_delta_time: &f64, canvas: &mut Canvas<Window>, player: &player::Player) {
+    let texture_creator = canvas.texture_creator();
+    let grass1 = texture_creator.load_texture("assets/grass1.png").unwrap();
+    let grass2 = texture_creator.load_texture("assets/grass2.png").unwrap();
     canvas.set_draw_color(Color::RGB(255, 255, 255));
     canvas.clear();
     for i in 0..(240 / TILE_SIZE) {
         for j in 0..(160 / TILE_SIZE) {
+            let render_quad = Rect::new(
+                i * TILE_SIZE,
+                j * TILE_SIZE,
+                TILE_SIZE as u32,
+                TILE_SIZE as u32,);
             if (i + j) % 2 == 0 {
-                canvas.set_draw_color(Color::RGB(255, 255, 255));
-                canvas
-                    .fill_rect(Rect::new(
-                        i * TILE_SIZE,
-                        j * TILE_SIZE,
-                        TILE_SIZE as u32,
-                        TILE_SIZE as u32,
-                    ))
-                    .unwrap();
+                canvas.copy(&grass1, None, render_quad)
+                .unwrap();
             } else {
-                canvas.set_draw_color(Color::RGB(255, 0, 0));
-                canvas
-                    .fill_rect(Rect::new(
-                        i * TILE_SIZE,
-                        j * TILE_SIZE,
-                        TILE_SIZE as u32,
-                        TILE_SIZE as u32,
-                    ))
-                    .unwrap();
+                canvas.copy(&grass2, None, render_quad).unwrap();
             }
         }
     }
