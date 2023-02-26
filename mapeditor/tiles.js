73f48e3f3cd0d43b5afac116b7e4afc492933933
Nathan Dodson <nathanrdodson@gmail.com>
2023-02-26 17:31:35 -0600

    Add new tilesprites

--- mapeditor/tiles.js
+++ mapeditor/tiles.js
@@ -148,6 +148,132 @@ const walls = [
         "y": 0,
         "hasCollision": true
     },
+    {
+        "name": "WINDOW",
+        "id": 4,
+        "x": 176,
+        "y": 0,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_L",
+        "id": 5,
+        "x": 96,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_M",
+        "id": 6,
+        "x": 112,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_R",
+        "id": 7,
+        "x": 128,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_S",
+        "id": 8,
+        "x": 144,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_HL",
+        "id": 9,
+        "x": 160,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_HR",
+        "id": 10,
+        "x": 176,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_TR",
+        "id": 11,
+        "x": 192,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_TL",
+        "id": 12,
+        "x": 208,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_DL",
+        "id": 13,
+        "x": 224,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_BL",
+        "id": 14,
+        "x": 240,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_DR",
+        "id": 15,
+        "x": 256,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "FENCE_BR",
+        "id": 16,
+        "x": 272,
+        "y": 16,
+        "hasCollision": true
+    },
+    {
+        "name": "TGRASS_1",
+        "id": 17,
+        "x": 0,
+        "y": 112,
+        "hasCollision": false
+    },
+    {
+        "name": "TGRASS_2",
+        "id": 18,
+        "x": 16,
+        "y": 112,
+        "hasCollision": false
+    },
+    {
+        "name": "TGRASS_3",
+        "id": 19,
+        "x": 32,
+        "y": 112,
+        "hasCollision": false
+    },
+    {
+        "name": "TGRASS_4",
+        "id": 20,
+        "x": 48,
+        "y": 112,
+        "hasCollision": false
+    },
+    {
+        "name": "TGRASS_5",
+        "id": 21,
+        "x": 80,
+        "y": 112,
+        "hasCollision": false
+    },
 ]
 const objects = [
     {
