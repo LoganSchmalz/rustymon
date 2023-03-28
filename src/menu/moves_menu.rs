1d8bf9012dddfc47bf6ef29639709c2846075f9c
TristanNiles <tristannilesmcculloch@gmail.com>
2023-03-28 18:20:32 -0500

    chnaged something in moves menu apparently

--- src/menu/moves_menu.rs
+++ src/menu/moves_menu.rs
@@ -25,19 +25,15 @@ impl MenuItem for MovesMenu {
         match action{ //check the user input to decide whether to activate a move or to scroll through moves
             MenuInput::Up => { //if user activates up input
                 self.selected = ((self.selected as i8 + 2) % 4) as usize;
-                println!("MOVED UP IN MENU");
             }
             MenuInput::Right => { //if user activates right input
                 self.selected = ((self.selected as i8 + 1) % 4) as usize;
-                println!("MOVED RIGHT IN MENU");
             }
             MenuInput::Down => { //if user activates down input
                 self.selected = ((self.selected as i8 - 2 + 4) % 4) as usize;
-                println!("MOVED DOWN IN MENU");
             }
             MenuInput::Left => { //if user activates left input
                 self.selected = ((self.selected as i8 - 1 + 4) % 4) as usize;
-                println!("MOVED LEFT IN MENU");
             }
             MenuInput::Accept => { //if user activates accept input
                 //activate that move
