ffb7687d7c2ad21c3215b55d7b4f1ba5052a78db
Logan Schmalz <loganschmalz@gmail.com>
2023-04-23 17:53:02 -0500

    Comment main

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ extern crate num_derive;
 use tilemap::TileMap;
 
 pub fn main() -> Result<(), String> {
+    //creating sdl context, window, canvas
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
 
@@ -40,35 +41,46 @@ pub fn main() -> Result<(), String> {
         .build()
         .map_err(|e| e.to_string())?;
 
+    //create sdl event pump
     let mut event_pump = sdl_context.event_pump().map_err(|e| e.to_string())?;
 
+    //create texture manager, font manager
     let texture_creator = canvas.texture_creator();
     let mut texture_manager = TextureManager::new(&texture_creator);
 
     let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
     let fonts = font_manager::Fonts::load(&ttf_context)?;
     let font_manager = font_manager::FontManager::new(fonts);
+
+    //create engine renderer
     let mut renderer = render::Renderer::new(canvas);
 
+    //load starting map
     let mut map = TileMap::load(0);
 
-    let mut time_now: u64 = sdl_context.timer().unwrap().performance_counter();
-
+    //create default gamestate
     let mut state = State {
         ..Default::default()
     };
 
+    //start timing frames
+    let mut time_now: u64 = sdl_context.timer().unwrap().performance_counter();
+
     'running: loop {
+        //calculate frametime
         let time_last = time_now;
         time_now = sdl_context.timer().unwrap().performance_counter();
         let delta_time: f32 = ((time_now - time_last) * 1000
             / sdl_context.timer().unwrap().performance_frequency())
             as f32;
 
+        //check if quit event happened
         if state.update_input(&mut event_pump, &mut renderer)? {
             break 'running;
         }
+        //update gamestate
         state.update(delta_time, &mut map, &font_manager)?;
+        //render game
         state.render(
             &mut renderer,
             &mut texture_manager,
